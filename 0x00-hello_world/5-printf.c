1898dc4fc2df6918d9ada40fc0f44cadb38c3497
Abesolom Nekahiwot <anekahiwot@gmail.com>
2022-03-10 12:59:22 -0800

    task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+nclude <stdio.h>
+
+/**
+ *  * main - Prints with proper grammer, but
+ *   * the outcome is a piece of art,followed by new line
+ *    *
+ *     * Return: Always 0.
+ *      */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+}
