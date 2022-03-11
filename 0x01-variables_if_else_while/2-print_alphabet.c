68c7a899bc95f6fca8c5d972201f417e2d957078
Abesolom Nekahiwot <anekahiwot@gmail.com>
2022-03-11 10:03:25 -0800

    task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,10 +6,10 @@
  *  Return: Always 0 (Success)
  */
 
-int main (void) {
+int main(void) {
 	char low;
 
-	for (low='a'; low <= 'z'; low++) 
+	for (low = 'a'; low <= 'z'; low++) 
 		putchar(low);
 	putchar('\n');
 	return (0);
