a693021161a62e72810c6336bb0ea62e3f40282b
Abesolom Nekahiwot <anekahiwot@gmail.com>
2022-03-15 14:28:26 -0700

    task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,7 +25,7 @@ void print_times_table(int n)
  *  * putformat - formatted characters to output
  *   * @n: number to format
  *    * Return: nothing
- *     */
+ */
 void putformat(int n)
 {
 		if (n <= 9)
