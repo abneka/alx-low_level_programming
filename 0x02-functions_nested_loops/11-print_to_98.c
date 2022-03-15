df810f5cab9a71fbcf371ddfeac360e92d11d64d
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2022-03-16 00:17:16 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,7 +6,7 @@
  *   * @n: A input integer
  *    * Description: function that prints all natural numbers
  *     * from n to 98, followed by a new line.
- *      * Return: Nothing
+ *       Return: Nothing
  */
 void print_to_98(int n)
 {
