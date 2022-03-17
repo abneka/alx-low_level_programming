92e2c8f28f4c7d1c86f00a5ab6773387ca734e01
Abesolom Nekahiwot <anekahiwot@gmail.com>
2022-03-17 13:38:31 -0700

    task

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,8 +9,8 @@ void print_most_numbers(void)
 {
 		int c;
 
-		for (c = 0; c <= 9; c++)
-			if (c != 2 && c != 4)
+		for (c = 48; c <= 57; c++)
+			if (c != 50 && c != 52)
 				_putchar(c);
 
 		_putchar('\n');
